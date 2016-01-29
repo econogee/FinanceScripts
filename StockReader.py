8090b33ed094c329c96eaec38e7892f9e937a075
Daniel Freeman <daniel.freeman@berkeley.edu>
2016-01-29 12:40:38 -0800

    Even more refactoring and bugtesting

--- StockReader.py
+++ StockReader.py
@@ -1,5 +1,7 @@
 #econogee, 1/28/2016
 #Stock Data Retrieval Script
+#If executed via the command line, will produce 500 data files with stock price information
+#between the dates specified in the main method.  Can also be imported to use the RetrieveStock method.
 
 import os
 import sys
@@ -8,8 +10,8 @@ import urllib2
 
 
 def RetrieveStock(TickerSymbol,start,end):
-	startday,startmonth,startyear = start
-	endday,endmonth,endyear = end
+	startday,startmonth,startyear = (str(s) for s in start)
+	endday,endmonth,endyear = (str(s) for s in end)
 	response = urllib2.urlopen('http://real-chart.finance.yahoo.com/table.csv?s='+str(TickerSymbol)+\
 								'&a=' + startday + '&b=' + startmonth + '&c=' + startyear + \
 								'&d=' + endday + '&e=' + endmonth + '&f=' + endyear + \
